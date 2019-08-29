5ef2533cdc9dfde802083270d06251196f0a8611
Fran Herruzo <franholmo@gmail.com>
2019-08-29 17:51:10 +0200

    Start FizzBuzz kata

---
+++ src/FizzBuzz/FizzBuzz.ts
@@ -0,0 +1,6 @@
+const FizzBuzz = (number: number) => {
+  if (number === 1) return 1;
+  return number;
+};
+
+export default FizzBuzz;
---
+++ tests/FizzBuzz/FizzBuzz.test.ts
@@ -0,0 +1,7 @@
+import FizzBuzz from '../../src/FizzBuzz/FizzBuzz';
+
+describe('FizzBuzz', () => {
+  it('should returns 1 when I fizzbuzz 1', () => {
+    expect(FizzBuzz(1)).toBe(1);
+  });
+});
