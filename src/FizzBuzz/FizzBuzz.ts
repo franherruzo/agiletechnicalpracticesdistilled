da9e160bc24f07472cba232034b4fc105c4c2c51
Fran Herruzo <franholmo@gmail.com>
2019-08-29 18:12:31 +0200

    Refactor fizzbuzz code

--- src/FizzBuzz/FizzBuzz.ts
+++ src/FizzBuzz/FizzBuzz.ts
@@ -1,7 +1,5 @@
 const FizzBuzz = (number: number): string => {
-  if (number === 15) return 'fizzbuzz';
-  if (number === 30) return 'fizzbuzz';
-  if (number === 45) return 'fizzbuzz';
+  if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz';
   if (number % 3 === 0) return 'fizz';
   if (number % 5 === 0) return 'buzz';
   return String(number);
