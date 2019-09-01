bc10dedcdcc0306f683b60de935aa78e863cbed1
Fran Herruzo <franholmo@gmail.com>
2019-09-01 16:06:33 +0200

    Check if is an atypical common year

--- src/LeapYear/LeapYear.ts
+++ src/LeapYear/LeapYear.ts
@@ -1,5 +1,5 @@
 const LeapYear = (year: number): boolean => {
-  if (year % 4 === 0) return true;
+  if (year % 4 === 0 && year % 100 !== 0) return true;
   return false;
 };
 
--- tests/LeapYear/LeapYear.test.ts
+++ tests/LeapYear/LeapYear.test.ts
@@ -7,4 +7,7 @@ describe('Leap Year', () => {
   it('check if 2001 a typical common year', () => {
     expect(LeapYear(2001)).toBe(false);
   });
+  it('check if 1900 is an atypical common year ', () => {
+    expect(LeapYear(1900)).toBe(false);
+  });
 });
