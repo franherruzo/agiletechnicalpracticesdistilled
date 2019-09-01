f4550e7c75771409186a86ac9e696ca6330a3744
Fran Herruzo <franholmo@gmail.com>
2019-09-01 16:50:27 +0200

    Added more assertions

--- src/Fibonacci/Fibonacci.ts
+++ src/Fibonacci/Fibonacci.ts
@@ -1,5 +1,7 @@
 const Fibonacci = (position: number): number => {
   if (position === 0) return 1;
+  if (position === 1) return 1;
+  if (position === 2) return 1;
   return 0;
 };
 
--- tests/Fibonacci/Fibonacci.test.ts
+++ tests/Fibonacci/Fibonacci.test.ts
@@ -1,7 +1,9 @@
 import Fibonacci from '../../src/Fibonacci/Fibonacci';
 
 describe('Fibonacci', () => {
-  it('return the next number(1) in the fibonacci sequence for the first position', () => {
+  it('return the next number(1) in the fibonacci sequence for the 1st, 2nd and 3rd position', () => {
     expect(Fibonacci(0)).toBe(1);
+    expect(Fibonacci(1)).toBe(1);
+    expect(Fibonacci(2)).toBe(1);
   });
 });
