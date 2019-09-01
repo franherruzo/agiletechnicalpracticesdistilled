df0ce81475400f7527661878e5e171a81ae9b717
Fran Herruzo <franholmo@gmail.com>
2019-09-01 16:50:44 +0200

    Refactor

--- src/Fibonacci/Fibonacci.ts
+++ src/Fibonacci/Fibonacci.ts
@@ -1,7 +1,5 @@
 const Fibonacci = (position: number): number => {
-  if (position === 0) return 1;
-  if (position === 1) return 1;
-  if (position === 2) return 1;
+  if (position < 3) return 1;
   return 0;
 };
 
