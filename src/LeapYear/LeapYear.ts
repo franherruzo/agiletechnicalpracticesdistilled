749efd8d7657241d821f439774499ee41276db4c
Fran Herruzo <franholmo@gmail.com>
2019-09-01 16:14:35 +0200

    Refactor conditional

--- src/LeapYear/LeapYear.ts
+++ src/LeapYear/LeapYear.ts
@@ -1,6 +1,4 @@
-const LeapYear = (year: number): boolean => {
-  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) return true;
-  return false;
-};
+const LeapYear = (year: number): boolean =>
+  year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
 
 export default LeapYear;
